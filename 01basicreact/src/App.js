1e29460f833e6974adec1ce378b641a9634665ba
Anurag Singhal <anuragsinghal1610@gmail.com>
2024-07-03 14:38:15 +0530

    Finished part 2 & 3 of series

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from './New.js'
+
+function App() {
+  return (
+    <>
+    <h1>Chai aur React | Abhishek Singhal</h1>
+    <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./new"
+
+function App() {
+  return (
+    <>
+      <h1>Chai aur React with Vite | Abhishek Singhal</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
